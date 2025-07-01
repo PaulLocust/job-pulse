8224ff4756da1b6884bae7bab2bc835a6a5e9c75
Pavel Sarancha <sarancha.pasha@mail.ru>
2025-07-02 01:12:14 +0300

    slog logger added

--- cmd/job-pulse/main.go
+++ cmd/job-pulse/main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"fmt"
 	"job-pulse/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+    envLocal = "local"
+    envDev   = "dev"
+    envProd  = "prod"
 )
 
 func main() {
 	fmt.Println("Hello, job-pulse!")
 	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting job-pulse", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
 	fmt.Println(cfg)
+}
+
+
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
